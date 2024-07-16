3c7eef3016b715d3beca52e6feb19949902c6588
Jon Daniel <jopadan@mailfence.com>
2024-07-16 08:09:27 +0200

    remove color code

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,13 +1,8 @@
 #include "NoSYS/glfw.hpp"
 
-col::u16<5,6,5> bfcolor = { 12, 23, 26 };
-col::u8<4>      vccolor = (col::u8<4>)bfcolor;
-
-
 int main(int argc, char** argv)
 {
 	sys::init();
-
 	std::cout << sys::view.test_cube;
 
 	while(sys::tick())
